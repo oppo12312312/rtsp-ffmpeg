dcfd4743a1a934cf60efd5363d06cc7584086b31
zhongshuai <zhongshuai@megvii.com>
2019-08-23 10:59:59 +0800

    143

--- mysocket.js
+++ mysocket.js
@@ -3,7 +3,7 @@
  * @Author: zhongshuai
  * @Date: 2019-08-22 17:36:58
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-08-22 22:21:17
+ * @LastEditTime: 2019-08-23 10:57:58
  */
 // 导入WebSocket模块:
 const WebSocket = require('ws');
@@ -11,10 +11,6 @@ const WebSocket = require('ws');
 // 引用Server类:
 const WebSocketServer = WebSocket.Server;
 
-// 实例化:
-const wss = new WebSocketServer({
-    port: 3000
-});
 
 //用户
 const user = null;
@@ -26,33 +22,75 @@ const mpeg1MuxerMap = {};
 
 let clients = {};
 
-wss.on('connection', function (ws) {
-    clients = this.clients;
-    console.log(this.clients.size);
-    ws.on('message', function (url) {
-        console.log(`[URL]  ${url}`);
-        if(!mpeg1MuxerMap[url]){
-            let mpeg1Muxer = new Mpeg1Muxer({
-                url: url
-            })
-            mpeg1Muxer.on('mpeg1data', (obj) => {
-                for (let client of clients) {
-                    if ( client.url === obj.url) {
-                        client.send(obj.data);
-                    }
-                }
-            })
-            mpeg1Muxer.on('ffmpegStderr', function(data) {
-                console.log(data.toString())
-            })
-            mpeg1MuxerMap[url] = mpeg1Muxer;
-        }
-        ws.url = url;
-    })
-   
+// // 实例化:
+// const wss = new WebSocketServer({
+//     port: 3000
+// });
 
+
+// wss.on('connection', function (ws) {
+//     clients = this.clients;
+//     console.log(this.clients.size);
+//     ws.on('message', function (url) {
+//         console.log(`[URL]  ${url}`);
+//         if(!mpeg1MuxerMap[url]){
+//             let mpeg1Muxer = new Mpeg1Muxer({
+//                 url: url
+//             })
+//             mpeg1Muxer.on('mpeg1data', (obj) => {
+//                 for (let client of clients) {
+//                     // console.log(client.url);
+//                     if ( client.url === obj.url) {
+//                         client.send(obj.data);
+//                     }
+//                 }
+//             })
+//             mpeg1Muxer.on('ffmpegStderr', function(data) {
+//                 console.log(data.toString())
+//             })
+//             mpeg1MuxerMap[url] = mpeg1Muxer;
+//         }
+//         ws.url = url;
+//     })
+
+// });
+
+const wss = new WebSocketServer({
+    port: 3000
 });
 
 
+wss.on('connection', function (ws) {
+    ws.on('message', function (url) {
+        console.log(url)
+        let mpeg1Muxer = new Mpeg1Muxer({
+            url: url
+        })
+        mpeg1Muxer.on('mpeg1data', (obj) => {
+            ws.send(obj.data);
+        })
+        mpeg1Muxer.on('ffmpegStderr', function(data) {
+            console.log(data.toString())
+        })
+    })
+});
+
+const wss1 = new WebSocketServer({
+    port: 3001
+});
 
 
+wss1.on('connection', function (ws) {
+    ws.on('message', function (url) {
+        console.log(url)
+        let mpeg1Muxer = new Mpeg1Muxer({
+            url: url
+        })
+        mpeg1Muxer.on('mpeg1data', (obj) => {
+            ws.send(obj.data);
+        })
+        mpeg1Muxer.on('ffmpegStderr', function(data) {
+            console.log(data.toString())
+        })
+    })
+});
