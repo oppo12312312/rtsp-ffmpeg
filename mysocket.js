7ea0460b9221ac42b4959ede742e5da134b1e45d
zhongshuai <zhongshuai@megvii.com>
2019-08-26 14:35:08 +0800

    m

--- mysocket.js
+++ mysocket.js
@@ -3,10 +3,12 @@
  * @Author: zhongshuai
  * @Date: 2019-08-22 17:36:58
  * @LastEditors: zhongshuai
- * @LastEditTime: 2019-08-23 16:22:24
+ * @LastEditTime: 2019-08-26 14:00:09
  */
 // 导入WebSocket模块:
+var _  = require('lodash');
 const WebSocket = require('ws');
+const qs = require('qs');
 
 // 引用Server类:
 const WebSocketServer = WebSocket.Server;
@@ -22,10 +24,6 @@ app.listen(8082, () => {
 //视屏流
 const Mpeg1Muxer = require('./mpeg1muxer')
 
-const mpeg1MuxerMap = {};
-
-let clients = {};
-
 
 
 // 实例化:
@@ -37,23 +35,27 @@ const wss = new WebSocketServer({
 
 
 
-wss.on('connection', function (ws) {
-    clients = this.clients;
-    console.log(this.clients.size)
-    let url = "rtsp://admin:admin@10.231.20.156/1"
+wss.on('connection', function (ws,req) {
+    console.log(this.clients.size);
+    const wsUrl = req.url;
+    let prarms = qs.parse(_.split(wsUrl,'?')[1]); // token=xxxxxx
+    const url = unescape(prarms.url);
+    console.log(`[URL]${url}`);
     let mpeg1Muxer = new Mpeg1Muxer({
         url: url
     })
-
-    mpeg1Muxer.on('ffmpegStderr', function(data) {
-        console.log(data.toString())
-    })
     mpeg1Muxer.on('mpeg1data', (obj) => {
-        console.log(clients.size)
-        for (let client of clients) {
-            client.send(obj.data);
-        }
+        ws.send(obj.data);
     })
+    mpeg1Muxer.on('ffmpegStderr', function(data) {
+        // console.log(data.toString())
+    })
+    ws.mpeg1Muxer = mpeg1Muxer;
+    ws.on("close", function(){
+        console.log("close");
+        ws.mpeg1Muxer.stream.kill();
+    })
+    
 });
 
 
